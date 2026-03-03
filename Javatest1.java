ac61cf0fab5852c7370172d649cf3a47815a9b96
Arthur Cavalcanti <84140217+Mystercup@users.noreply.github.com>
2026-03-03 10:42:37 -0300

    Add files via upload

---
+++ Javatest1.java
@@ -0,0 +1,30 @@
+public class Javatest1{
+    public static void main(String[] args){
+
+// System.out.println() and System.out.println(String.format())
+
+        System.out.println("Hello World");
+        String sinopse;
+        sinopse = "Filme legal";
+        System.out.println(String.format("olha que %s", sinopse));
+
+// Format
+        int a = 5;
+        double b = 1.5;
+
+// Casting
+        int soma = (int)b + a;
+
+        System.out.println("O número é =" + soma);
+
+
+// atividade 1 
+        double nota1 = 7.5;
+        double nota2 = 9.5;
+
+        double notamed = (nota1 + nota2)/2;
+        System.out.println("sua média foi = " + notamed);
+    }
+
+
+}
